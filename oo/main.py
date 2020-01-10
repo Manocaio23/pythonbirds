2c45bbd217dc9d8b6de8e6c926b89abd7025a8bc
ManoCaio <caio19augusto@outlook.com>
2020-01-09 21:02:03 -0300

    criado atributos de instância nome e idade

--- oo/main.py
+++ oo/main.py
@@ -1,5 +1,11 @@
 from oo.pessoa import Pessoa
 
-p=Pessoa()
+
+p=Pessoa('Caio')
 
 print(p.cumprimentar())
+print(p.nome)
+p.nome='Max'
+print(p.nome)
+print(p.idade)
+
--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,10 @@
 class Pessoa():
+
+    def __init__(self, nome=None, idade=35):#seria o moetodo contrutor. nome do parametro caso
+        self.idade = idade
+        self.nome=nome
+
+
+
     def cumprimentar(self):
         return 'oi'
